f9cd8228a6620be256c39b37fc9d16987970c8f6
Gustavo D'Angelo <gustavo.vonn@gmail.com>
2023-11-28 12:00:55 -0300

    a

--- versao_final/main.py
+++ versao_final/main.py
@@ -20,9 +20,21 @@ clock = pg.time.Clock()
 screen = pg.display.set_mode((c.SCREEN_WIDTH + c.PANEL_SIZE, c.SCREEN_HEIGHT))
 pg.display.set_caption("Tower Defense")
 
+#Para criar textos
+text_font = pg.font.SysFont("Arial", 30)
+
+def draw_text(text, font, text_color, x, y):
+  img = font.render(text, True, text_color)
+  screen.blit(img, (x, y))
+
+
+########################################################Isso aqui deve dar pra ficar só dentro de play()
 ultimo_spawn_inimigo = pg.time.get_ticks()
 placing_turrets = False
 selected_turret = None
+########################################################
+
+
 
 #mapa
 map_image = pg.image.load('levels/default.png').convert_alpha()
@@ -45,6 +57,17 @@ enemy_image3 = pg.image.load('assets/imagens/inimigos/enemy_3.png').convert_alph
 buy_turret_image = pg.image.load('assets/imagens/botoes/buy_turret.png').convert_alpha()
 cancel_image = pg.image.load('assets/imagens/botoes/cancel.png').convert_alpha()
 upgrade_turret_image = pg.image.load('assets/imagens/botoes/upgrade_turret.png').convert_alpha()
+
+  #tela de inicio
+wallpaper_image = pg.image.load('assets/imagens/wallpaper.jpeg').convert_alpha()
+new_game_button_image = pg.image.load('assets/imagens/botoes/newgame.png').convert_alpha()
+leave_button_image = pg.image.load('assets/imagens/botoes/leave.png').convert_alpha()
+records_button_image = pg.image.load('assets/imagens/botoes/records.png').convert_alpha()
+
+  #pause
+pause_image = pg.image.load('assets/imagens/componentes/pause.png').convert_alpha()
+close_button_image = pg.image.load('assets/imagens/botoes/close.png').convert_alpha()
+back_button_image = pg.image.load('assets/imagens/botoes/back.png').convert_alpha()
 
 #pegando o arquivo json para usar como fase:
 with open('levels/default.tmj') as file:
@@ -111,84 +134,127 @@ enemy_group = pg.sprite.Group()
 turret_group = pg.sprite.Group()
 
 
-
-
 #create buttons 
 turret_button = Button(c.SCREEN_WIDTH + 30, 120, buy_turret_image, True)
 cancel_button = Button(c.SCREEN_WIDTH + 50, 180, cancel_image, True)
 upgrade_button = Button(c.SCREEN_WIDTH + 5, 180, upgrade_turret_image, True)
+new_game_button = Button(c.SCREEN_WIDTH + 50, 300, new_game_button_image, True)
+close_button = Button(c.SCREEN_WIDTH +80, 370, close_button_image, True)
+back_button = Button(c.SCREEN_WIDTH, 370, back_button_image, True)
 
 #game loop
-run = True
-while run:
+def play():
+  ultimo_spawn_inimigo = pg.time.get_ticks()
+  placing_turrets = False
+  selected_turret = None
+  run = True
+  while run:
 
-  clock.tick(c.FPS)
+    clock.tick(c.FPS)
 
-  # Updating
+    # Updating
 
-  #atualizar grupos
-  enemy_group.update()
-  turret_group.update(enemy_group)
+    #atualizar grupos
+    enemy_group.update()
+    turret_group.update(enemy_group)
 
-  if selected_turret:
-    selected_turret.selected = True
+    if selected_turret:
+      selected_turret.selected = True
 
-  # Drawing
+    # Drawing
 
-  screen.fill("grey100")
+    screen.fill("grey100")
 
-  #desenharlevel
-  world.draw(screen)
+    #desenharlevel
+    world.draw(screen)
 
-  #desenhargrupos
-  enemy_group.draw(screen)
-  turret_group.draw(screen)
-  for turret in turret_group:
-    turret.draw(screen)
-
-
-  #spawnar inimigos
-  if pg.time.get_ticks() - ultimo_spawn_inimigo >= c.SPAWN_COOLDOWN and world.inimigos_spawnados < len(world.lista_inimigos):
-    enemy_group.add(decidir_tipo_inimigo())
-    ultimo_spawn_inimigo = pg.time.get_ticks()
+    #desenhargrupos
+    enemy_group.draw(screen)
+    turret_group.draw(screen)
+    for turret in turret_group:
+      turret.draw(screen)
 
 
-  #desenhar botoes
-  if turret_button.draw(screen):
-    placing_turrets = True
-  if placing_turrets == True:
-    cursor_rect = cursor_turret.get_rect()
-    cursor_pos = pg.mouse.get_pos()
-    cursor_rect.center = cursor_pos
-    if cursor_pos[0] <= c.SCREEN_WIDTH:
-      screen.blit(cursor_turret, cursor_rect)
-    if cancel_button.draw(screen):
-      placing_turrets = False
-  
-  #se um turret for selecionado:
-  if selected_turret and selected_turret.nivel < 3:
-    if upgrade_button.draw(screen):
-      turret_group.remove(selected_turret)
-      turret_group.add(upgrade_turret(selected_turret, turret_sheet2, turret_sheet3))
-      selected_turret = None
+    #spawnar inimigos
+    if pg.time.get_ticks() - ultimo_spawn_inimigo >= c.SPAWN_COOLDOWN and world.inimigos_spawnados < len(world.lista_inimigos):
+      enemy_group.add(decidir_tipo_inimigo())
+      ultimo_spawn_inimigo = pg.time.get_ticks()
 
-  #event handler
-  for event in pg.event.get():
+
+    #desenhar botoes
+    if turret_button.draw(screen):
+      placing_turrets = True
+    if placing_turrets == True:
+      cursor_rect = cursor_turret.get_rect()
+      cursor_pos = pg.mouse.get_pos()
+      cursor_rect.center = cursor_pos
+      if cursor_pos[0] <= c.SCREEN_WIDTH:
+        screen.blit(cursor_turret, cursor_rect)
+      if cancel_button.draw(screen):
+        placing_turrets = False
     
-    if event.type == pg.QUIT:
-      run = False
-    
-    if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
-      mouse_pos = pg.mouse.get_pos()
-      if mouse_pos[0] < c.SCREEN_WIDTH and mouse_pos[1] < c.SCREEN_HEIGHT:
+    #se um turret for selecionado:
+    if selected_turret and selected_turret.nivel < 3:
+      if upgrade_button.draw(screen):
+        turret_group.remove(selected_turret)
+        turret_group.add(upgrade_turret(selected_turret, turret_sheet2, turret_sheet3))
         selected_turret = None
-        clear_selection()
-        if placing_turrets == True:
-          create_turret(mouse_pos)
-        else:
-            selected_turret = select_turret(mouse_pos)
+
+    #event handler
+    for event in pg.event.get():
       
-  #update display
-  pg.display.flip()
+      if event.type == pg.QUIT:
+        run = False
+      
+      if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+        mouse_pos = pg.mouse.get_pos()
+        if mouse_pos[0] < c.SCREEN_WIDTH and mouse_pos[1] < c.SCREEN_HEIGHT:
+          selected_turret = None
+          clear_selection()
+          if placing_turrets == True:
+            create_turret(mouse_pos)
+          else:
+              selected_turret = select_turret(mouse_pos)
+
+      if event.type == pg.KEYDOWN:
+        if event.key == pg.K_ESCAPE:
+          pause()
+
+
+
+    #update display
+    pg.display.flip()
+
+
+#Para pausar
+def pause():
+  paused = True
+
+  while paused:
+    for event in pg.event.get():
+      if event.type == pg.QUIT:
+        pg.quit()
+        quit()
+    
+      if event.type == pg.KEYDOWN:
+        if event.key == pg.K_c:
+          paused = False
+        elif event.key == pg.K_q:
+          pg.quit()
+          quit()
+
+      if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+        mouse_pos = pg.mouse.get_pos()
+        if mouse_pos[0] == back_button.x and mouse_pos[1] == back_button.y:
+          paused = False
+    screen.fill('blue')
+    back_button.draw(screen)
+    close_button.draw(screen)
+
+    pg.display.update()
+    clock.tick(5)
+
+
+play()
 
 pg.quit()
