d955879be8a31247729204c0a2b19319fd2cab2e
nuvme <pedrofmm13@gmail.com>
2023-12-04 22:46:26 -0300

    set init state

--- versao_final/run.py
+++ versao_final/run.py
@@ -1,8 +1,6 @@
 from controllers.game import GameController
 
-from states.init import InitState
-
 if __name__ == "__main__":
     game = GameController()
-    game.change_state(InitState)
+    game.set_init_state()
     game.run()
